cdddc9b57b0400f48ee868bf6690b41209e66470
MyCallAngel0 <mihail.mihalachi@isa.utm.md>
2025-05-05 21:54:48 +0300

    Fix deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@vitejs/plugin-tailwindcss';
-import ghPages from 'vite-plugin-gh-pages';
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), ghPages()],
+  plugins: [react(), tailwindcss()],
   base: '/tasktrackr/',
 });
